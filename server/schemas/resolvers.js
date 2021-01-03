8f2655886606c2716f9022fdfe62ebaa3d9d61ac
aksmith5239 <aklobby@gmail.com>
2021-01-02 17:57:02 -0700

    user and book schemas started

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,23 @@
+const { User, Book } = require('../models');
+
 const resolvers = {
     Query: {
-        bookWorm:() => {
-            return 'Welcome to BookWorm';
+        books: async (parent, { title }) => {
+            const params = title ? { title } : {};
+            return Book.find(params);
+            }, 
+        books: async (parent, { bookId }) => {
+            return Book.findOne({ bookId });
+            },
+         users: async () => {
+             return User.find()
+             .select('-__v, -password');
+         }, 
+         users: async (parent, {username}) => {
+             return User.findOne({username})
+             .select('-__v, -password');
+         }   
         }
-    }
-}
+    };
+
 module.exports = resolvers;
