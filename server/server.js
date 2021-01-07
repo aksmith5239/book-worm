642af9c21fc13de704de133c7cb0e36281472430
aksmith5239 <aklobby@gmail.com>
2021-01-07 07:50:34 -0700

    updated server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
 const path = require('path');
 const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./Schemas');
+const { authMiddleware } = require('./utils/auth');
 // const routes = require('./routes');
-const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
